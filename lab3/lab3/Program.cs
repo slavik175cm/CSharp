686670f953d74cc774d3fc411d068e5e8b0c2ec5
slavik175cm <61192458+slavik175cm@users.noreply.github.com>
2020-03-25 01:37:42 +0300

    started third lab

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle a = new Vehicle("A012B12", 100, 200, 150, 2500, new DateTime(2002, 10, 10), true);
+            Vehicle b = new Vehicle("AAAAAAA", 50, 100, 100, 1500, false);
+            Console.WriteLine(a.manufacturingTime);
+            Console.WriteLine(b.manufacturingTime);
+
+
+
+        }
+    }
+}
---
+++ lab3/lab3/Vehicle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab3
+{
+    class Vehicle
+    {
+        public string serialNumber { get; private set; }
+        public double weight { get; private set; }
+        public double carryingCapacity { get; private set; }
+        public double maxSpeed { get; private set; }
+        public double cost { get; private set; }
+        public DateTime manufacturingTime { get; private set; }
+        public bool isBroken { get; private set; }
+        public uint myid { get; private set; }
+        private static uint id = 0;
+
+        public Vehicle(string serialNumber, double weight, double carryingCapacity, double maxSpeed, double cost, bool isBroken = false)
+        {
+            this.serialNumber = serialNumber;
+            this.weight = weight;
+            this.carryingCapacity = carryingCapacity;
+            this.maxSpeed = maxSpeed;
+            this.cost = cost;
+            this.manufacturingTime = DateTime.Now;
+            this.isBroken = isBroken;
+            this.myid = ++id;
+        }
+
+        public Vehicle(string serialNumber, double weight, double carryingCapacity, double maxSpeed, double cost, DateTime manufacturingTime, bool isBroken = false)
+        {
+            this.serialNumber = serialNumber;
+            this.weight = weight;
+            this.carryingCapacity = carryingCapacity;
+            this.maxSpeed = maxSpeed;
+            this.cost = cost;
+            this.manufacturingTime = manufacturingTime;
+            this.isBroken = isBroken;
+            this.myid = ++id;
+        }
+
+        public bool ChangeSerialNumber(string newSerialNumber)
+        {
+            foreach(var ch in newSerialNumber)
+            {
+                if ((ch < 'A' || ch > 'Z') && (ch < '0' || ch > '9'))
+                {
+                    return false;
+                }
+            }
+            serialNumber = newSerialNumber;
+            return true;
+        }
+
+        public int HowManyYears()
+        {
+            return DateTime.Now.Year - manufacturingTime.Year;
+        }
+
+        public void Repair()
+        {
+            isBroken = false;
+        }
+
+    }
+}
