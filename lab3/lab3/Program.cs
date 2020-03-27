cefa0540ed6a3ebc3c44b37449af5fd766d5be68
slavik175cm <61192458+slavik175cm@users.noreply.github.com>
2020-03-28 01:46:04 +0300

    made menu

--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -9,83 +9,262 @@ namespace lab3
             var vehicles = new Dictionary<int, Vehicle>();
             vehicles[1] = new Vehicle("A012B12", 100, 200, 150, 2500, 10000, new DateTime(2002, 10, 10), true);
             vehicles[2] = new Vehicle("CC8D091", 50, 100, 100, 1500);
-
-            //Vehicle a = new Vehicle("A012B12", 100, 200, 150, 2500, 10000, new DateTime(2002, 10, 10), true);
-            //Vehicle b = new Vehicle("CC8D091", 50, 100, 100, 1500);
-            Console.WriteLine(vehicles[1].InfoToString());
-            Console.WriteLine(vehicles[2].InfoToString());
+            bool doClear = false;
             while (true)
             {
-                Console.WriteLine("Input 1 to show all the vehicles");
-                Console.WriteLine("Input 2 to add a new vehicle");
-                Console.WriteLine("Input 3 to delete vehicle by id");
-                Console.WriteLine("Input 4 to change some vehicle");
-                Console.WriteLine("Input 5 to quit the program");
+                Console.WriteLine("1 to show all the vehicles");
+                Console.WriteLine("2 to add a new vehicle");
+                Console.WriteLine("3 to delete vehicle by id");
+                Console.WriteLine("4 to change some vehicle");
+                Console.WriteLine("5 to quit the program");
                 switch(Choice(1, 5))
                 {
                     case 1:
-                        if (vehicles.Count == 0)
-                        {
-                            Console.WriteLine("There is no vehicles");
-                        }
-                        foreach (var a in vehicles)
-                        {
-                            Console.WriteLine(a.Value.InfoToString());
-                        }
+                        Console.Clear();
+                        ShowAll(vehicles);
+                        doClear = false;
                         break;
                     case 2:
-
+                        Console.Clear();
+                        vehicles[Vehicle.Id + 1] = GetNew();
+                        Console.WriteLine("Vehicle has been added!");
+                        doClear = true;
                         break;
                     case 3:
                         DeleteVehicle(vehicles);
+                        doClear = true;
                         break;
                     case 4:
+                        Console.Clear();
+                        ChangeSome(vehicles);
+                        doClear = true;
                         break;
                     case 5:
                         return;
                 }
                 Console.ReadLine();
+                if (doClear) Console.Clear();
             }
         }
 
-        static int Choice(int min = int.MinValue, int max = int.MaxValue)
+        static int Choice(int min = int.MinValue, int max = int.MaxValue, string text = "Choice: ")
         {
             int x;
-            Console.Write("Choice: ");
+            Console.Write(text);
             while (!int.TryParse(Console.ReadLine(), out x) || x < min || x > max)
             {
                 Console.WriteLine("Error! Input a number from {0} to {1}", min, max);
-                Console.Write("Choice: ");
+                Console.Write(text);
             }
             Console.WriteLine();
             return x;
         }
 
-        static void DeleteVehicle(Dictionary<int, Vehicle> vehicles)
+        static int ReadInt(string s)
         {
-            Console.WriteLine("Choose the id of the vehicle you want to delete");
+            int x;
+            Console.Write(s);
+            while (!int.TryParse(Console.ReadLine(), out x))
+            {
+                Console.WriteLine("String is not an integer! Try again");
+                Console.Write(s);
+            }
+            return x;
+        }
+
+        static double ReadDouble(string text)
+        {
+            double x;
+            Console.Write(text);
+            while (!double.TryParse(Console.ReadLine().Replace('.', ','), out x))
+            {
+                Console.WriteLine("String is not a number! Try again");
+                Console.Write(text);
+            }
+            return x;
+        }
+
+        static void ShowAll(Dictionary<int, Vehicle> vehicles)
+        {
+            if (vehicles.Count == 0)
+            {
+                Console.WriteLine("There is no vehicles");
+            } 
+            else
+            {
+                foreach (var a in vehicles)
+                {
+                    Console.Write(a.Value.InfoToString());
+                    Console.ReadLine();
+                }
+                Console.WriteLine("That's all of them");
+            }
+        }
+
+        static Vehicle GetNew()
+        {
+            string serialNumber;
+            while (true)
+            {
+                bool ok = true;
+                Console.Write("Serial number - ");
+                serialNumber = Console.ReadLine();
+                if (serialNumber.Length > 20) ok = false;
+                foreach(var ch in serialNumber)
+                {
+                    if ((ch < 'A' || ch > 'Z') && (ch < '0' || ch > '9'))
+                        ok = false;
+                }
+                if (ok) break;
+                Console.WriteLine("String should contain up to 20 symbols A-Z and 0-9");
+            }
+            double weight = ReadDouble("Weight(kg) - ");
+            double carryingCapacity = ReadDouble("Carrying capacity(kg) - ");
+            double maxSpeed = ReadDouble("MaxSpeed(m/s) - ");
+            double cost = ReadDouble("Cost($) - ");
+            while (true)
+            {
+                Console.Write("Is your vehicle new(y/n)? "); 
+                string input = Console.ReadLine().ToLower();
+                if (input == "y")
+                {
+                    return new Vehicle(serialNumber, weight, carryingCapacity, maxSpeed, cost);
+                }
+                if (input == "n")
+                {
+                    break;
+                }
+            }
+            double mileage = ReadDouble("Mileage(m) - ");
+            bool isBroken;
+            while (true)
+            {
+                Console.Write("Is your vehicle broken(y/n)? ");
+                string input = Console.ReadLine().ToLower();
+                if (input == "y")
+                {
+                    isBroken = true;
+                    break;
+                }
+                if (input == "n")
+                {
+                    isBroken = false;
+                    break;
+                }
+            }
+            Console.WriteLine("Manufacturing time");
+            DateTime manufacturingTime;
+            while (true)
+            {
+
+                try
+                {
+                    manufacturingTime = new DateTime(Choice(1, 9999, "Year - "), Choice(1, 12, "Mounth - "), Choice(1, 30, "Day - "));
+                    if (manufacturingTime < DateTime.Now)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Is the vehicle from the future?");
+                } catch
+                {
+                    Console.WriteLine("Such date is not exist!");
+                }
+            }
+                return new Vehicle(serialNumber, weight, carryingCapacity, maxSpeed, cost, mileage, manufacturingTime, isBroken);
+
+        }
+
+        static int ChooseSpecific(Dictionary<int, Vehicle> vehicles)
+        {
             int choice;
             while (true)
             {
-                choice = Choice();
+                choice = Choice(0);
                 if (vehicles.ContainsKey(choice))
                 {
-                    vehicles.Remove(choice);
-                    Console.WriteLine("It has been removed!");
-                    break;
+                    return choice;
                 } 
                 else
                 {
                     Console.WriteLine("There is no vehicle with such id! Type 'y' to continue...");
                     if (Console.ReadLine().ToLower() != "y")
                     {
-                        Console.WriteLine("Nothing has been deleted");
-                        break;
+                        return -1;
                     }
                 }
             }
         }
 
+        static void DeleteVehicle(Dictionary<int, Vehicle> vehicles)
+        {
+            Console.WriteLine("Choose the id of the vehicle you want to delete");
+            int index = ChooseSpecific(vehicles);
+            if (index == -1)
+            {
+                Console.WriteLine("Nothing has been deleted");
+                return;
+            }
+            vehicles.Remove(index);
+            Console.WriteLine("It has been removed!");
+        }
+
+        static void ChangeSome(Dictionary<int, Vehicle> vehicles)
+        {
+            Console.WriteLine("Choose the id of the vehicle you want to change");
+            int index = ChooseSpecific(vehicles);
+            if (index == -1)
+            {
+                Console.WriteLine("Nothing has been changed");
+                return;
+            }
+            Console.WriteLine("1 to change the serial number");
+            Console.WriteLine("2 to run some kilometers");
+            Console.WriteLine("3 to repair/break the vehicle");
+            Console.WriteLine("4 to do nothing");
+            switch(Choice(1, 4))
+            {
+                case 1:
+                    Console.Write("New serial number - ");
+                    while (!vehicles[index].ChangeSerialNumber(Console.ReadLine()))
+                    {
+                        Console.WriteLine("String should contain up to 20 symbols A-Z and 0-9");
+                        Console.Write("New serial number - ");
+                    }
+                    Console.WriteLine("Serial number has been changed");
+                    break;
+                case 2:
+                    Console.WriteLine("Input the distance you want to drive");
+                    vehicles[index].Run(ReadDouble("Length(m) = "));
+                    Console.WriteLine("OK! Current mileage is {0}", vehicles[index].Mileage);
+                    break;
+                case 3:
+                    Console.WriteLine("1 to repair");
+                    Console.WriteLine("2 to break it");
+                    int choice = Choice(1, 2);
+                    if (choice == 1 && vehicles[index].IsBroken == false)
+                    {
+                        Console.WriteLine("It's already not broken");
+                        break;
+                    }
+                    if (choice == 2 && vehicles[index].IsBroken == true)
+                    {
+                        Console.WriteLine("It's already broken");
+                        break;
+                    }
+                    if (choice == 1)
+                    {
+                        vehicles[index].Repair();
+                        Console.WriteLine("it's has been repaired");
+                        break;
+                    }
+                    vehicles[index].Break();
+                    Console.WriteLine("it's has been broken");
+                    break;
+                case 4:
+                    Console.WriteLine("Nothing has been changed");
+                    return;
+            }
+        }
 
     }
 }
--- lab3/lab3/Vehicle.cs
+++ lab3/lab3/Vehicle.cs
@@ -14,8 +14,8 @@ namespace lab3
         public double Mileage { get; private set; }
         public DateTime ManufacturingTime { get; private set; }
         public bool IsBroken { get; private set; }
-        public uint MyId { get; private set; }
-        public static uint Id = 0;
+        public int MyId { get; private set; }
+        public static int Id = 0;
 
         public Vehicle(string serialNumber, double weight, double carryingCapacity, double maxSpeed, double cost)
         {
@@ -40,6 +40,7 @@ namespace lab3
 
         public bool ChangeSerialNumber(string newSerialNumber)
         {
+            if (newSerialNumber.Length > 20) return false;
             foreach(var ch in newSerialNumber)
             {
                 if ((ch < 'A' || ch > 'Z') && (ch < '0' || ch > '9'))
@@ -55,14 +56,14 @@ namespace lab3
         {
             StringBuilder info = new StringBuilder();
             info.AppendLine("******************************************************");
-            info.AppendLine(String.Format("Id: {0}", MyId));
-            info.AppendLine(String.Format("Serial number: {0}", SerialNumber));
-            info.AppendLine(String.Format("Weight: {0}kg", Weight));
-            info.AppendLine(String.Format("Carrying capacity: {0}kg", CarryingCapacity));
-            info.AppendLine(String.Format("Max speed: {0}m/s", MaxSpeed));
-            info.AppendLine(String.Format("Mileage: {0}m/s", Mileage));
-            info.AppendLine(String.Format("Cost: {0}$", Cost));
-            info.AppendLine(String.Format("Manufacturing time: {0}, {1} year(s)", ManufacturingTime.ToShortDateString(), GetHowManyYears().ToString()));
+            info.AppendLine($"Id: {MyId}");
+            info.AppendLine($"Serial number: {SerialNumber}");
+            info.AppendLine($"Weight: { Weight}kg");
+            info.AppendLine($"Carrying capacity: {CarryingCapacity}kg");
+            info.AppendLine($"Max speed: {MaxSpeed}m/s");
+            info.AppendLine($"Mileage: {Mileage}m");
+            info.AppendLine($"Cost: {Cost}$");
+            info.AppendLine($"Manufacturing time: { ManufacturingTime.ToShortDateString()}, {GetHowManyYears().ToString()} year(s)");
             info.AppendLine(IsBroken ? "Vehicle is broken!" : "Vehicle is not broken");
             return info.ToString();
         }
